11525d7221a7729ba4fe1d9ed46b28e98cfd294a
johbry17 <73319490+johbry17@users.noreply.github.com>
2023-08-10 02:22:36 -0400

    queries 6 of 8 complete

--- queries_for_analysis.sql
+++ queries_for_analysis.sql
@@ -28,10 +28,36 @@ JOIN dept_manager AS dm ON dm.emp_no = e.emp_no
 JOIN departments AS d ON d.dept_no = dm.dept_no;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT
+	de.dept_no,
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM employees AS e
+JOIN dept_emp AS de ON de.emp_no = e.emp_no
+JOIN departments AS d ON d.dept_no = de.dept_no;
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT
+	first_name,
+	last_name,
+	sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT
+	emp_no,
+	last_name,
+	first_name
+FROM employees
+WHERE emp_no IN(
+	SELECT emp_no FROM dept_emp  WHERE dept_no IN(
+		SELECT dept_no FROM departments WHERE dept_name = 'Sales'
+	)	
+);
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
